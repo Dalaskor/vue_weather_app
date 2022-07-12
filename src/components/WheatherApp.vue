a2e3d59da735bb68ebf2296dccfe53ca1ff6fcb7
Dalaskor <dalas.prog@gmail.com>
2022-07-12 09:53:30 +0400

    init

---
+++ src/components/WheatherApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'INIT',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
